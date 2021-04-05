8613b191cdd1e7ac002dc21e231ba4561284c57e
Petr Savichev (proton) <psavichev@gmail.com>
2021-04-05 10:12:32 -0500

    started project notes sync

--- src/logic.js
+++ src/logic.js
@@ -14,6 +14,7 @@ export default class GrabberLogic {
 	static async sync(lib, argv) {
 		const todoistApi = todoist(argv.todoistToken);
 		await this.syncProjects(lib, todoistApi);
+		await this.syncProjectNotes(lib, todoistApi);
 		await this.syncContexts(lib, todoistApi);
 		await this.syncTasks(lib, todoistApi);
 	}
@@ -25,17 +26,38 @@ export default class GrabberLogic {
 	static async syncProjects(doitLib, todoistApi) {
 		await todoistApi.sync();
 
-		const doitProjects = await doitLib.getProjects();
-		const doitProjectNames = Object.values(doitProjects).map(this.formatName);
-		doitProjectNames.push('Inbox');
+		let doitProjects = await doitLib.getProjects();
+		doitProjects.push({ name: 'Inbox' });
 		
 		const todoistProjects = todoistApi.projects.get();
-		const todoistProjectNames = todoistProjects.map(project => project.name);
-		
-		const missingProjectNames = [...new Set(doitProjectNames.filter(name => !todoistProjectNames.includes(name)))];
 
-		for (const name of missingProjectNames) {
-			await todoistApi.projects.add({ name: name });
+		for (const doitProject of doitProjects) {
+			const todoistProject = todoistProjects.find(p => this.formatName(p.name) == this.formatName(doitProject.name));
+			if (!todoistProject) {
+				await todoistApi.projects.add({ name: this.formatName(doitProject.name) });
+			}
+		}
+
+		await todoistApi.commit();
+	}
+
+	static async syncProjectNotes(doitLib, todoistApi) {
+		await todoistApi.sync();
+
+		let doitProjects = await doitLib.getProjects();
+		
+		const todoistProjects = todoistApi.projects.get();
+		const todoistProjectNotes = todoistApi.projectNotes.get();
+
+		for (const doitProject of doitProjects) {
+			const todoistProject = todoistProjects.find(p => this.formatName(p.name) == this.formatName(doitProject.name));
+			if (todoistProject && doitProject.notes) {
+				const todoistProjectNote = todoistProjectNotes.find(n => n.project_id == todoistProject.id)
+				if (!todoistProjectNote) {
+					// Error: An invalid sync command was sent
+					// await todoistApi.projectNotes.add({ content: doitProject.notes.replace(/\s+/g, ' '), project_id: todoistProject.id });
+				}
+			}
 		}
 
 		await todoistApi.commit();
@@ -82,8 +104,7 @@ export default class GrabberLogic {
 		const todoistTasks = todoistApi.items.get();
 		const todoistNotes = todoistApi.notes.get();
 
-		console.log(doitTasks);
-		console.log(doitTasks.length);
+		// console.log(doit=.length);
 
 		await todoistApi.commit();
 	}
